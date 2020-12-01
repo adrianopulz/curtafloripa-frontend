41ba43168ab4ef0cab13afae331508b78280d1d0
Adriano Pulz <adrianopulz@gmail.com>
2020-11-30 23:55:45 -0300

    Under Construct page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Logo from "../components/Logo"
+import "../components/global.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div className={"logo"}>
+        <Logo />
+        <p>Em Construção.</p>
+      </div>
+    </div>
+  )
 }
