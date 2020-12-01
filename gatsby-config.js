df0fee6566bd08f1f4cefb7e5afe1e31c66d0e85
Adriano Pulz <adrianopulz@gmail.com>
2020-12-01 00:31:09 -0300

    Fixed readme and Manifest infos.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `curta-floripa`,
+        short_name: `curta-floripa`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#113c4a`,
+        theme_color: `#3f7b70`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
