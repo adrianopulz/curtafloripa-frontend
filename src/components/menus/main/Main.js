4ce00a8ec8ee2f5969575d213ad8da69e6bf59e0
Adriano Pulz <adrianopulz@gmail.com>
2020-12-29 11:21:59 -0300

    Home page mobile and hamburger menu.

--- src/components/menus/main/Main.js
+++ src/components/menus/main/Main.js
@@ -1,18 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import "../menu.scss"
 import "./main.scss";
+import { classNames } from "@splidejs/react-splide/src/js/utils"
 
 export default () => {
+  const [menuStatus, setMenuStatus] = useState(false);
+
+  const navClasses = classNames({
+    'menu': true,
+    'opened': menuStatus
+  });
+
   return (
-    <nav className={"menu"} id={"main-menu"}>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/praias">Praias</Link></li>
-        <li><Link to="/">Trilhas</Link></li>
-        <li><Link to="/">Onde Ficar</Link></li>
-      </ul>
-    </nav>
+    <>
+      <button id={"hamburger"} onClick={ () => setMenuStatus(true) }><i>MENU</i></button>
+      <nav className={ navClasses } id={"main-menu"}>
+        <button id={"closeMenu"} onClick={ () => setMenuStatus(false) }>X</button>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/praias">Praias</Link></li>
+          <li><Link to="/">Trilhas</Link></li>
+          <li><Link to="/">Onde Ficar</Link></li>
+        </ul>
+      </nav>
+    </>
   );
 }
