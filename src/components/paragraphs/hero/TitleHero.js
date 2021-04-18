b7bf726cd4b01cabe04defe66907ae1242218f1d
Adriano Pulz <adrianopulz@gmail.com>
2021-04-18 20:36:58 -0300

    Responsive fixes and removing old image.

--- src/components/paragraphs/hero/TitleHero.js
+++ src/components/paragraphs/hero/TitleHero.js
@@ -1,22 +1,13 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 import "./title-hero.scss"
 
-const TitleHero = ({ title }) => {
+const TitleHero = ({ title, image }) => {
   return (
     <article id={"title-hero"}>
       <div className={"container"}>
-        <StaticImage
-          src="../../../assets/images/construcao-bg.jpg"
-          alt="Praia com a areia"
-          placeholder="blurred"
-          layout="fixed"
-          width={400}
-          height={300}
-        />
-
-        <h2>Praias</h2>
+        { image }
+        <h1>{ title }</h1>
       </div>
     </article>
   );
