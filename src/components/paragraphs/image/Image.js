c41b39536e5c62b93fa37d76423aa58f3199b9bc
Adriano Pulz <adrianopulz@gmail.com>
2021-04-25 22:00:25 -0300

    Fixed the max breakpoint for image paragraph in 1088px.

--- src/components/paragraphs/image/Image.js
+++ src/components/paragraphs/image/Image.js
@@ -28,7 +28,7 @@ export const fragment = graphql`
           media: field_media_image {
             file: localFile {
               childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+                gatsbyImageData(quality: 100, layout: FULL_WIDTH, breakpoints: [750, 1088])
               }
             }
           }
