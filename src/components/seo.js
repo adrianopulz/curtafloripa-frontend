ddf3cc8e03b895897190ed7687fd30ce38752d6a
Adriano Pulz <adrianopulz@gmail.com>
2021-04-28 00:09:41 -0300

    Changes on SEO component.

--- src/components/seo.js
+++ src/components/seo.js
@@ -26,27 +26,25 @@ const SEO = ({ title, description, image, article }) => {
 
   return (
     <Helmet htmlAttributes={{ lang: 'pt' }} title={seo.title} titleTemplate={titleTemplate}>
-      <meta charSet="utf-8" />
       <link rel="canonical" href={siteUrl} />
       <meta name="description" content={seo.description} />
       <meta name="keywords" content="floripa, florianopolis, ilha, magia, praia, trilha, litoral, sul, brasil" />
       <meta name="image" content={seo.image} />
-      {seo.url && <meta property="og:url" content={seo.url} />}
+
       {(article ? true : null) && <meta property="og:type" content="article" />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
-      {seo.description && (
-        <meta property="og:description" content={seo.description} />
-      )}
-      {seo.image && <meta property="og:image" content={seo.image} />}
+
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={seo.image} />
       {twitterUsername && (
         <meta name="twitter:creator" content={twitterUsername} />
       )}
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-      {seo.description && (
-        <meta name="twitter:description" content={seo.description} />
-      )}
-      {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   )
 }
